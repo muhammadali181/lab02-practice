d1106b0f1ad5d091e343f5e33a6790d71b6ba7f8
muhammadali181 <291264182@formanite.fccollege.edu.pk>
2026-02-24 07:46:50 +0500

    Maths tools and its testing file has been updated

--- math_tools.py
+++ math_tools.py
@@ -4,3 +4,26 @@ def multiply(a,b):
     return a*b
 def is_even(n):
     return n%2 == 0
+def subtract(a,b):
+    return a - b
+def max_of_three(a,b,c):
+    if (a>b) and (a>c):
+        return a
+    elif (b>a) and (b>c):
+        return b
+    elif a==b==c:
+        return "All these numbers are equal"
+    else:
+        return c
+def is_palindrome(s):
+    if len(s) <= 1:
+        return True
+    elif s == s[::-1]:
+        return True
+    return False
+def find_min(numbers):
+    min_val = numbers[0]
+    for i in range(1, len(numbers)):
+        if numbers[i] > min_val:
+            min_val = numbers[i]
+    return min_val
--- test_math_tools.py
+++ test_math_tools.py
@@ -1,4 +1,4 @@
-from math_tools import add, multiply, is_even
+from math_tools import add, multiply, is_even, subtract, max_of_three, is_palindrome, find_min
 
 def test_add():
     assert add (2,3) == 5
@@ -18,7 +18,39 @@ def test_is_even():
     assert is_even(0) == True
     print("test_is_even : ALL PASSED")
 
+def test_subtract():
+    assert subtract(4,0) == 4
+    assert subtract(8,9) == -1
+    assert subtract(0,0) == 0
+    print("test_subtract : ALL PASSED")
+
+def test_max_of_three():
+    assert max_of_three(0,1,2) == 2
+    assert max_of_three(5,6,8) == 8
+    assert max_of_three(3,3,3) == "All these numbers are equal"
+    assert max_of_three(2,2,5) == 5
+    assert max_of_three(4,1,9) == 9
+    print("test_max_of_three : ALL PASSED")
+
+def test_is_palindrome():
+    assert is_palindrome("madam") == True
+    assert is_palindrome("racecar") == True
+    assert is_palindrome("car") == False
+    assert is_palindrome("i") == True
+    assert is_palindrome("") == True
+    print("test_is_palindrome : ALL PASSED")
+def test_find_min():
+    assert find_min([1,2,3,4]) == 1
+    assert find_min([2,5,6,8]) == 2
+    assert find_min([9,8,7,6]) == 6
+    print("test_find_min : ALL PASSED")
+
 test_add()
 test_multiply()
 test_is_even()
+test_subtract()
+test_max_of_three()
+test_is_palindrome()
+test_find_min()
+
 print("--- All tests passed ---")
